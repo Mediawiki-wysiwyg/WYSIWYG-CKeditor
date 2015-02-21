c02a7ae01a8b6e79ef577c04059d1b5114c2487e
Varlin <Varlin@users.noreply.github.com>
2015-02-21 13:16:21 +0100

    Add language variables for template dialog "titles" tags

--- WYSIWYG/ckeditor/plugins/mwtemplate/dialogs/template.js
+++ WYSIWYG/ckeditor/plugins/mwtemplate/dialogs/template.js
@@ -17,7 +17,7 @@ CKEDITOR.dialog.add( 'MWTemplate', function( editor ) {
                             type: 'textarea',
                             rows: 16, 
                             label: editor.lang.mwtemplateplugin.defineTmpl,
-                            title: 'Template Tag definition',
+                            title: editor.lang.mwtemplateplugin.defineTmplTitle,
                             className: 'swmf_class',
                             style: 'border: 1px;'
                         }
